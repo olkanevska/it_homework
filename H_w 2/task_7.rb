4cdb28eecd9525e84a9869edb3f9f6d8257056b2
olkanevska <olkanevska@gmail.com>
2017-08-08 16:01:28 +0300

    Corrected task_7 in H_w 2

--- H_w 2/task_7.rb
+++ H_w 2/task_7.rb
@@ -1,13 +1,13 @@
 # 7. There is an array of integers (ary). Modify it with adding ary[0] (first element of the array) to each even number. Don't do it for first and last elements
 
-arr = [1, 2, 3, 4, 5, 6, 7, 8, 9,10]
+arr = [2, 2, 3, 2, 5, 6, 7, 8, 9,10]
 
-y = arr[0]
-z = arr[-1]
-arr.map! do |x|
-  if x.even? && x!= y && x!= z
-    x += y
-  else x+= 0
+a=arr.index(arr[-1])
+
+arr.each_with_index do | item,index |
+  if item.even? && index!=0 && index != a
+    v = arr[index]+ arr[0]
+    arr[index] = v
   end
 end
 puts "#{arr}"
