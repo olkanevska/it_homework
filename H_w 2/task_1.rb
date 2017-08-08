d2a7c87c6598d51a7703aa6d3fe8b5b754139287
olkanevska <olkanevska@gmail.com>
2017-08-08 08:25:44 +0300

    Corrected task_1 in H_w 2

--- H_w 2/task_1.rb
+++ H_w 2/task_1.rb
@@ -1,5 +1,9 @@
 #1. Create a method which will take a natural number as an argument and will find a sum of all its digits
-def sum a,b
-  a+b
-end
-puts sum 1,4
+ print "Write a natural number and press Enter: "
+ a = gets.strip
+ 
+ arr=a.to_s.split''
+ arr.map!{ |e| e.to_i}
+
+ sum = arr.inject(0){ |result, elem| result + elem }
+ puts "Sum of all argument's digits: #{sum}"
