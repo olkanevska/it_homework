f99336aea53ed040b92dd399d72988419ff6ba47
olkanevska <olkanevska@gmail.com>
2017-08-08 09:43:02 +0300

    Corrected task_8 in H_w 2

--- H_w 2/task_8.rb
+++ H_w 2/task_8.rb
@@ -1,7 +1,5 @@
-=begin 8. There is a hash where keys and values are strings.
-Modify it: all keys should be symbols and all values should be integers.
-If you couldn't modify some value set it to nil
-=end
+#8. There is a hash where keys and values are strings. Modify it: all keys should be symbols and all values should be integers. If you couldn't modify some value set it to nil
+
 hash = {'Dima'=>'30', 'Lena'=>'31'}
 
 new_hash = Hash[ hash.map { |k, v| [k.to_sym, v.to_i] } ]
