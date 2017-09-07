f00ee385e2daa371b81cfce3c9bef7e356448325
olkanevska <olkanevska@gmail.com>
2017-09-07 14:20:40 +0300

    Corrected task_8 in H_w 2

--- H_w 2/task_8.rb
+++ H_w 2/task_8.rb
@@ -1,7 +1,12 @@
 #8. There is a hash where keys and values are strings. Modify it: all keys should be symbols and all values should be integers. If you couldn't modify some value set it to nil
 
-hash = {'Dima'=>'30', 'Lena'=>'31'}
+
+hash = {'Dima'=>'30', 'Lena'=>'31','Igor'=> 'boy'}
 
 hash = Hash[ hash.map { |k, v| [k.to_sym, v.to_i] } ]
+
+hash.each_pair do |k,v|
+  hash[k] = nil if v == 0
+end
 
 puts hash
