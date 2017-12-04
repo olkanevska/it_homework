0a3bbb1ddf7ea9fd8e4b284c47c041908e5b52aa
Olena <olkanevska@gmail.com>
2017-12-04 14:02:35 +0200

    add def open_bug_issue

--- H_w 5/our_module.rb
+++ H_w 5/our_module.rb
@@ -155,4 +155,12 @@ module OurModule
 
   end
 
+  def open_bug_issue
+    @driver.find_element(:class,'my-page').click
+    @wait.until {@driver.find_element(:class,'mypage-box').displayed?}
+
+    classes_els = @driver.find_elements(:css,'.subject > a')
+    @my_bug_issue = classes_els.find {|el| el.text.include? @bug_issue}
+  end
+  
 end
